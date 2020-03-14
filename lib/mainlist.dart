ec1d0c1d49e484c96c8f7f1ece71ffa2dc2ae852
tipej <tiitus.jarvinen@gmail.com>
2020-03-14 14:25:22 +0200

    Added tap to launch news url

--- lib/mainlist.dart
+++ lib/mainlist.dart
@@ -1,6 +1,7 @@
 import 'package:coronapp/models/models.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'resources/resources.dart';
 
 class NewsListProvider extends ChangeNotifier {
@@ -44,39 +45,10 @@ class MainList extends StatelessWidget {
                   return Dismissible(
                     key: Key(item.url),
                     onDismissed: (direction) {
-                      // TODO: Implement options (save, share etc)
+                      // TODO: Implement options (save, share, etc.)
                       provider.removeItemAt(i);
                     },
-                    child: InkWell(
-                      child: Padding(
-                        padding: const EdgeInsets.all(5.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(item.title, style: Theme.of(context).textTheme.subhead),
-                            const SizedBox(height: 2.5),
-                            Text(
-                              item.description, 
-                              style: Theme.of(context).textTheme.body1.apply(color: Colors.grey[700]),
-                              maxLines: 3,
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                            Text.rich(
-                              TextSpan(
-                                style: Theme.of(context).textTheme.overline,
-                                children: <InlineSpan>[
-                                  TextSpan(text: "${item.source} - "),
-                                  item.published != null ? TextSpan(
-                                    text: item.published.formatString()
-                                  ) : null,
-                                ].nonNulls(),
-                              )
-                            )
-                          ]
-                        )
-                      ),
-                      onTap: () {},
-                    )
+                    child: _NewsItem(item: item)
                   );
                 },
               ),
@@ -84,6 +56,56 @@ class MainList extends StatelessWidget {
           },
         ),
       ),
+    );
+  }
+}
+
+class _NewsItem extends StatelessWidget {
+  const _NewsItem({
+    Key key,
+    @required this.item,
+  }) : super(key: key);
+
+  final NewsPiece item;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      child: Padding(
+        padding: const EdgeInsets.all(5.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(item.title, style: Theme.of(context).textTheme.subhead),
+            const SizedBox(height: 2.5),
+            Text(
+              item.description, 
+              style: Theme.of(context).textTheme.body1.apply(color: Colors.grey[700]),
+              maxLines: 3,
+              overflow: TextOverflow.ellipsis,
+            ),
+            Text.rich(
+              TextSpan(
+                style: Theme.of(context).textTheme.overline,
+                children: <InlineSpan>[
+                  TextSpan(text: "${item.source} - "),
+                  item.published != null ? TextSpan(
+                    text: item.published.formatString()
+                  ) : null,
+                ].nonNulls(),
+              )
+            )
+          ]
+        )
+      ),
+      onTap: () async {
+        final url = item.url;
+        if (await canLaunch(url)) {
+          await launch(url);
+        } else {
+          throw "Couldn't launch $url";
+        }
+      },
     );
   }
 }
